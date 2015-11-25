00cf024729c360a89e5c823ad1cdce60122f9a50
unknown <t2vuka00@PELISTUDIO-01.kk.lan>
2015-11-25 15:33:27 +0200

    3,4 kenttä sekä AI ja spawn ja videot ja menu
    ads

---
+++ TestRunProject-18.11.2015/Assets/Jape/PlayerAttacking.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+public class PlayerAttacking : MonoBehaviour
+
+{
+    public int attackDamage = 10;
+    public float range = 3f;
+    Ray attackRay;
+    RaycastHit attackHit;
+    EnemyHealth enemyHealth;
+    GameObject Enemy;
+
+    void Awake()
+    {
+        
+       // enemyHealth = Enemy.GetComponent<EnemyHealth>();
+      //  enemyHealth = GetComponent<EnemyHealth>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Lumbering"))
+        {
+            Attack();
+        }
+    }
+
+   
+
+    void Attack()
+    {
+
+
+        if (Physics.Raycast(attackRay, out attackHit, range))
+        {
+            EnemyHealth enemyHealth = attackHit.collider.GetComponent<EnemyHealth>();
+
+            if (enemyHealth != null)
+                enemyHealth.TakeDamage(attackDamage, attackHit.point);
+        }
+    }
+}
+//{
+//    public GameObject enemy;
+//    public float attackTimer;
+//    public float coolDown;
+//   // EnemyHealth enemyHealth;
+//    // Use this for initialization
+//    void Start()
+//    {
+//        attackTimer = 0;
+//        coolDown = 2.0f;
+//    }
+
+//    // Update is called once per frame
+//    void Update()
+//    {
+//        if (attackTimer > 0)
+//            attackTimer -= Time.deltaTime;
+
+//        if (attackTimer < 0)
+//            attackTimer = 0;
+
+//        if (Input.GetKeyUp(KeyCode.F))
+//        {
+//            if (attackTimer == 0)
+//            {
+//                Attack();
+//                attackTimer = coolDown;
+//            }
+//        }
+
+//    }
+
+//    private void Attack()
+//    {
+//        float distance = Vector3.Distance(enemy.transform.position, transform.position);
+
+//        Vector3 dir = (enemy.transform.position - transform.position).normalized;
+
+//        float direction = Vector3.Dot(dir, transform.forward);
+
+//        if (distance < 2.5f)
+//        {
+//            if (direction > 0)
+//            {
+//                EnemyHealth eh = (EnemyHealth)enemy.GetComponent("EnemyHealth");
+//                eh.setCurrentHealth(-10);
+//            }
+//        }
+//    }
+//}
---
+++ TestRunProject-18.11.2015/Assets/STARTMENU/Scripts/LevelScripts/TESTPLAYER.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TESTPLAYER : MonoBehaviour
+{
+        public float speed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+    void FixedUpdate()
+    {
+        float moveVertical;
+        float moveHorizontal;
+        moveVertical = Input.GetAxis("Vertical");
+        moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        GetComponent<Rigidbody>().AddForce(movement * speed * Time.deltaTime);
+    }
+
+}
+
