5e4bdf8335a088e0701510966d46b05bd40fce70
Laikkaaboss <tompah2@hotmail.com>
2015-12-04 10:34:01 +0200

    Lopputekstit jee

---
+++ TestRunProject-18.11.2015/Assets/Credits.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+    public GameObject camera;
+    public int speed = 1;
+
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+        camera.transform.Translate(Vector3.down * Time.deltaTime * speed);
+        StartCoroutine(waitFor());
+                }
+    IEnumerator waitFor(){
+        yield return new WaitForSeconds(50);
+        Application.LoadLevel(0);
+        
+        }
+}
