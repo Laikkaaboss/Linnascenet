026877f134200a21cba63a692650ee1018aa92b9
t2vuka00 <kallevuorinen@hotmail.com>
2015-12-15 10:44:37 +0200

    Väliscenepohjat

---
+++ TestRunProject-18.11.2015/Assets/Scripts/Rotator.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Rotator : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+	}
+	// Update is called once per frame
+	void Update()
+	{
+		transform.Rotate(new Vector3(0, 0, 15) * Time.deltaTime);
+	}
+}
